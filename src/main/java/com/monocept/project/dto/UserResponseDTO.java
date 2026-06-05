75e5e2b27038fdaad7baae28a300d25078d7c5c4
Peddi Vydehi <vydehipeddi@gmail.com>
2026-06-05 16:48:16 +0530

    added

--- src/main/java/com/monocept/project/dto/UserResponseDTO.java
+++ src/main/java/com/monocept/project/dto/UserResponseDTO.java
@@ -1,5 +1,23 @@
 package com.monocept.project.dto;
 
+import com.monocept.project.enums.Role;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponseDTO {
 
+    private Long userId;
+    private String fullName;
+    private String email;
+    private String mobileNumber;
+    private Role role;
+    private Boolean activeStatus;
+    private LocalDateTime createdDate;
 }
