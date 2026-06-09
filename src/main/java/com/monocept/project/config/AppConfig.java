f31ac0df8178aedfc50d6263076b21c9f4b92a92
Nishtha Srivastava <nistha25srivastava@gmail.com>
2026-06-09 15:18:33 +0530

    nth change

--- src/main/java/com/monocept/project/config/AppConfig.java
+++ src/main/java/com/monocept/project/config/AppConfig.java
@@ -4,18 +4,36 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.monocept.project.dto.CustomerResponseDTO;
+import com.monocept.project.model.Customer;
+
 @Configuration
 public class AppConfig {
 
     @Bean
     public ModelMapper modelMapper() {
+
         ModelMapper mapper = new ModelMapper();
-        
-        // Ensures strict property matching to avoid mismatching nested IDs
-        mapper.getConfiguration()
-              .setFieldMatchingEnabled(true)
-              .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
-              
+
+        mapper.typeMap(Customer.class, CustomerResponseDTO.class)
+                .addMappings(m -> {
+
+                    m.map(
+                        src -> src.getUser().getFullName(),
+                        CustomerResponseDTO::setFullName
+                    );
+
+                    m.map(
+                        src -> src.getUser().getEmail(),
+                        CustomerResponseDTO::setEmail
+                    );
+
+                    m.map(
+                        src -> src.getUser().getMobileNumber(),
+                        CustomerResponseDTO::setMobileNumber
+                    );
+                });
+
         return mapper;
     }
 }
